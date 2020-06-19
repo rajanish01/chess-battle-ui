f339fd58a5f28c10c01567efea4a9eaa4bc7af9b
Rajanish <rajanish.ranjan@acornmachine.io>
2020-06-20 04:08:18 +0530

    added user profile sidebar skeleton
    Signed-off-by: Rajanish <rajanish.ranjan@acornmachine.io>

---
+++ user_profie/user_profile.js
@@ -0,0 +1,38 @@
+jQuery(function($) {
+
+  $(".sidebar-dropdown > a").click(function() {
+    console.log("i work")
+    $(".sidebar-submenu").slideUp(200);
+    if (
+      $(this)
+      .parent()
+      .hasClass("active")
+    ) {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .parent()
+        .removeClass("active");
+    } else {
+      $(".sidebar-dropdown").removeClass("active");
+      $(this)
+        .next(".sidebar-submenu")
+        .slideDown(200);
+      $(this)
+        .parent()
+        .addClass("active");
+    }
+  });
+
+  $("#close-sidebar").click(function() {
+    console.log("i work")
+    $(".page-wrapper").removeClass("toggled");
+  });
+  $("#show-sidebar").click(function() {
+    console.log("i work")
+    $(".page-wrapper").addClass("toggled");
+  });
+
+
+
+
+});
